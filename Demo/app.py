0ce453c2c5da33de59ba239fa8b7b9a73e5a9495
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-10-13 12:44:10 -0600

    stop

--- Demo/app.py
+++ Demo/app.py
@@ -107,6 +107,7 @@ def model_fit(model):
 #    history = model_fit(model)
 #    df = pd.DataFrame(history.history)
 #    st.line_chart(df)
+st.stop()
 model = keras.models.load_model('mi_modelo.h5')
 st.markdown('''
 #### Ahora que la red neuronal ha sido entrenada podemos ponerla a prueba entregandole algunas imagenes de numeros para ver que haga la prediccion correcta
