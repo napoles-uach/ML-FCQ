7dc5c6a86c958cf0a81a6f81712217c613f3333a
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2023-01-08 18:14:41 -0600

    Create canvOpt.py

---
+++ Demo/canvOpt.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from scipy.ndimage.interpolation import zoom
+from streamlit_drawable_canvas import st_canvas
+
+# Load trained model
+model = tf.keras.models.load_model('Demo/mi_modelo.h5')
+
+def process_image(image_data, size=28):
+  """Convert drawn image to grayscale and resize to 28x28."""
+  # Convert image to grayscale
+  grayscale_image = np.sum(image_data, axis=2)
+  # Resize image
+  resized_image = zoom(grayscale_image, size / grayscale_image.shape[0])
+  # Normalize pixel values
+  normalized_image = resized_image.astype(np.float32) / 255
+  # Return image as a single row
+  return normalized_image.reshape(1, -1)
+
+with st.expander('Draw a digit:'):
+  # Display canvas for drawing
+  canvas_result = st_canvas(stroke_width=10, height=28*5, width=28*5)
+  
+  # Process drawn image and make prediction using model
+  if canvas_result.image_data is not None:
+    # Convert drawn image to grayscale and resize to 28x28
+    processed_image = process_image(canvas_result.image_data)
+    # Make prediction using model
+    prediction = model.predict(processed_image).argmax()
+    # Display prediction
+    st.header('Prediction:')
+    st.write('This image appears to be a ' + str(prediction) + '.')
